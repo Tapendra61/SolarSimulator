42f2365540f39620cabace80dc488fe3890086cf
Tapendra Shahi <tapendrashahi61@gmail.com>
2026-05-28 23:57:13 +0530

    test: tests for EventBus

---
+++ tests/SolarSim.Core.Tests/Events/EventBusTests.cs
@@ -0,0 +1,49 @@
+using Xunit;
+using SolarSim.Core.Events;
+
+namespace SolarSim.Core.Tests.Events;
+
+public class EventBusTests
+{
+	[Fact]
+	public void PublishDeliversEventToSubscriber()
+	{
+		EventBus bus = new();
+		int receivedValue = 0;
+		bus.Subscribe<TestEvent>(evt => receivedValue = evt.Value);
+
+		bus.Publish(new TestEvent(44));
+
+		Assert.Equal(44, receivedValue);
+	}
+
+	[Fact]
+	public void PublishDoesNotDeliverToUnSubscribed()
+	{
+		EventBus bus = new();
+		int callCount = 0;
+		void Handler(TestEvent evt) => callCount++;
+		bus.Subscribe<TestEvent>(Handler);
+
+		bus.Unsubscribe<TestEvent>(Handler);
+		bus.Publish(new TestEvent(1));
+
+		Assert.Equal(0, callCount);
+	}
+
+	[Fact]
+	public void PublishDeliversToMultipleSubscribers()
+	{
+		EventBus bus = new();
+
+		int firstCalls = 0;
+		int secondCalls = 0;
+
+		bus.Subscribe<TestEvent>(_ => firstCalls++);
+		bus.Subscribe<TestEvent>(_ => secondCalls++);
+
+		bus.Publish(new TestEvent(1));
+		Assert.Equal(1, firstCalls);
+		Assert.Equal(1, secondCalls);
+	}
+}
